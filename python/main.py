7d36612f0fe1bc3fd65525e9060395ad2734a04c
oleksiiredko <oredko@contractor.worldremit.com>
2021-11-19 00:16:09 +0200

    fix condition or

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ print("Hi, this is wizard to setup your lab environment")
 
 user_choice = str(input("type 'yes' to proceed or enter any key for cancelation: "))
 
-if user_choice == 'yes' or 'Yes' or 'YES':
+if user_choice == 'yes' or user_choice == 'Yes' or user_choice == 'YES':
     print('your choice YES! Lets GO')
 else:
     print('all right lets do that manually')
