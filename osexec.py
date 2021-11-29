46c4a1db116c46a0214391083e9482b4c8f252a9
oleksiiredko <redisav@gmail.com>
2021-11-29 15:04:08 +0200

    modified check_terr_install func

--- osexec.py
+++ osexec.py
@@ -9,16 +9,16 @@ mac_terraform_url = 'https://releases.hashicorp.com/terraform/1.0.11/terraform_1
 
 # Defining functions
 
-def check_terr_install(terraform_url):  #<<----CHECK IT  !!!!!! Передать значения переменных в функцию
-    if os.system('terraform --version') == 0:  # В зависимости от операционной системы
+def check_terr_install(terraform_url,unzip,terraform_path):
+    if os.system('terraform --version') == 0:
         print("Terraform already installed")
     else:
         print("Terraform is not installed")
         user_choice = str(input("type 'yes' to proceed with install or enter any key for cancel: "))
         if user_choice == 'yes' or user_choice == 'Yes' or user_choice == 'YES':
             os.system(terraform_url)
-            os.system('unzip terraform_*.zip')
-            os.system('mv ./terraform /usr/local/bin/')
+            os.system(unzip)
+            os.system(terraform_path)
         else:
             print("Try to install Terraform manually")
 
@@ -34,8 +34,10 @@ if os_platform_name == 'Darwin':
     check_terr_install(terraform_url_mac)
 elif os_platform_name == 'Linux':
     print("this is a Linux OS")
-    y_terraform_url = 'wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip'
-    check_terr_install(terraform_url)
+    linux_terraform_url = 'wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip'
+    linux_unzip = 'unzip terraform_*.zip'
+    linux_terraform_path = 'mv ./terraform /usr/local/bin/'
+    check_terr_install(linux_terraform_url,linux_unzip,linux_terraform_path)
 elif os_platform_name == 'nt':
     print("this is a Windows OS")
     os.chdir("../terraform")
