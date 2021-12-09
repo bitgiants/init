3529659551bf093314294afc7ba5278b905c10e6
oleksiiredko <oredko@contractor.worldremit.com>
2021-12-10 00:09:29 +0200

    added terraform init/plan/apply fetures to init script

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 import os
 import platform
 
-terr_path = "../../terraform"
+ter_path = "../../terraform/"
 ter_vars = "../../terraform/variables.tf"
 vsphere_user_name = str('default = "vsphere_user_name"')
 win_terraform_url = 'https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_windows_amd64.zip'
@@ -17,8 +17,11 @@ def check_terr_install(terraform_url, unzip, terraform_path):
         print("Terraform is not installed")
         user_choice = str(input("type 'yes' to proceed with install or enter any key for cancel: "))
         if user_choice == 'yes' or user_choice == 'Yes' or user_choice == 'YES':
+            # download terraform binary
             os.system(terraform_url)
+            # unzip terraform binary to workdir
             os.system(unzip)
+            # mv terraform binary to os binary directory
             os.system(terraform_path)
         else:
             print("Try to install Terraform manually")
@@ -36,9 +39,13 @@ if os_platform_name == 'Darwin':
     check_terr_install(terraform_url_mac)
 elif os_platform_name == 'Linux':
     print("this is a Linux OS")
+    # download terraform binary:
     linux_terraform_url = 'wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip'
+    # unzip terraform binary to workdir:
     linux_unzip = 'unzip terraform_*.zip'
+    # mv terraform binary to os binary directory
     linux_terraform_path = 'mv ./terraform /usr/local/bin/'
+    # check terraform installation and install if not:
     check_terr_install(linux_terraform_url, linux_unzip, linux_terraform_path)
 elif os_platform_name == 'Windows':
     print("this is a Windows OS")
@@ -61,7 +68,8 @@ elif os_platform_name == 'Windows':
             if not os.path.isfile(filepath):
                 print(f'Unzip terraform bin into {install_dirpath}')
                 archive_path = r'.\terraform.zip'
-                win_unzip = str(f'{power_shell} Expand-Archive -LiteralPath {archive_path} -DestinationPath {install_dirpath}')
+                win_unzip = str(
+                    f'{power_shell} Expand-Archive -LiteralPath {archive_path} -DestinationPath {install_dirpath}')
                 os.system(win_unzip)
         else:
             print(os.environ.get('PATH'))
@@ -69,7 +77,7 @@ else:
     print("Unreconized or unsupported OS")
 
 
-def get_user_data(data_name):
+def get_user_data(data_name):  # function to get user input
     return input(f"Enter your  {data_name} : ")
 
 
@@ -95,14 +103,17 @@ if user_choice == 'yes' or user_choice == 'Yes' or user_choice == 'YES':
 else:
     print('all right lets do that manually')
 
-is_exec_t_init = str(input("type 'yes' if you would like to execute terraform init and plan or enter any key for cancelation: "))
-if is_exec_t_init == 'yes' or is_exec_t_init == 'Yes' or is_exec_t_init == 'YES':
-    os.system(f'powershell.exe cd ..\\..\\terraform')
-    print(os.system('powershell.exe pwd'))
-    print(os.system('terraform init'))
-    print(os.system('terraform plan'))
+should_exec_t_init = str(input("type 'yes(y)' if you would like to execute terraform init and plan and or enter "
+                               "any key for cancelation: "))
+if should_exec_t_init == 'yes' or should_exec_t_init == 'Yes' or should_exec_t_init == 'YES' or should_exec_t_init == 'y':
+    print(os.system(f'terraform -chdir="{ter_path}" init'))
+    print(os.system(f'terraform -chdir="{ter_path}" plan'))
 else:
     print('ok, next time!!!')
 
-
-
+should_exec_t_apply = str(input("type 'yes(y)' if you would like to execute terraform apply or enter "
+                                "any key for cancelation: "))
+if should_exec_t_apply == 'yes' or should_exec_t_apply == 'Yes' or should_exec_t_apply == 'YES' or should_exec_t_init == 'y':
+    print(os.system(f'terraform -chdir="{ter_path}" apply'))
+else:
+    print('ok, next time!!!')
