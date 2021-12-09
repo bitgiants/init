1700c73550a561a0ba592d863d1e0d57f34d687e
oleksiiredko <oredko@contractor.worldremit.com>
2021-12-09 11:27:53 +0200

    update init for windows

--- osexec.py
+++ osexec.py
@@ -22,6 +22,7 @@ def check_terr_install(terraform_url, unzip, terraform_path):
         else:
             print("Try to install Terraform manually")
 
+
 if os_platform_name == 'Darwin':
     print("this is a mac OS")
     os.chdir("../terraform")
@@ -38,15 +39,30 @@ elif os_platform_name == 'Linux':
     linux_unzip = 'unzip terraform_*.zip'
     linux_terraform_path = 'mv ./terraform /usr/local/bin/'
     check_terr_install(linux_terraform_url, linux_unzip, linux_terraform_path)
-elif os_platform_name == 'nt':
+elif os_platform_name == 'Windows':
     print("this is a Windows OS")
-    #os.chdir("../terraform")
-    win_terraform_url = 'Invoke-WebRequest https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_windows_amd64.zip -OutFile terraform.zip'
-    win_unzip = 'Expand-Archive -LiteralPath .\terraform.zip -DestinationPath 'C:\Program Files (x86)\''
-    #$ENV:PATH
-    #'$ENV:PATH="$ENV:PATH;'   
-    win_terraform_path =()
-    check_terr_install(win_terraform_url, win_unzip, win_terraform_path)
-   
+    if os.system('terraform --version') == 0:
+        print("Terraform already installed")
+    else:
+        print("Terraform is not installed")
+        win_user_choice = str(input("type 'yes' to proceed with install or enter any key for cancel: "))
+        if win_user_choice == 'yes' or win_user_choice == 'Yes' or win_user_choice == 'YES':
+            print('Download Terraform binary')
+            power_shell = '"powershell.exe"'
+            # This block is ok, tested and working---------------------------------------------------
+            win_terraform_url = str('Invoke-WebRequest '
+                                    'https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0'
+                                    '.11_windows_amd64.zip -OutFile terraform.zip')
+            download_terraform = f'{power_shell} {win_terraform_url}'
+            os.system(download_terraform)
+            install_dirpath = 'C:\\Windows\\System32'
+            filepath = f'{install_dirpath}\\terraform.exe'
+            if not os.path.isfile(filepath):
+                print(f'Unzip terraform bin into {install_dirpath}')
+                archive_path = r'.\terraform.zip'
+                win_unzip = str(f'{power_shell} Expand-Archive -LiteralPath {archive_path} -DestinationPath {install_dirpath}')
+                os.system(win_unzip)
+        else:
+            print(os.environ.get('PATH'))
 else:
     print("Unreconized or unsupported OS")
